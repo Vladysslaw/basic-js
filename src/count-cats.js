037c3ee472162021e8f9b7582b8092975bec83ca
Vlad <vladuha011@mail.ru>
2021-03-05 21:44:13 +0300

    init: dream-team & count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
+  let ears = 0;
+  matrix.forEach(arr => {
+    arr.forEach(item => {
+      if (item == '^^') {
+        ears++;
+      }
+    });
+  });
+  return ears;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
   // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let arr = [];
+  members.forEach(name => {
+    /* if (Array.isArray(name)) {
+      name = name.join('');
+    } */
+    if (typeof(name) == 'string') {
+      for (let i = 0; i < name.length; i++) {
+        if (name[i] != ' ') {
+          arr.push(name[i]);
+          break;
+        }
+      }
+    }
+  });
+  arr = arr.join('').toUpperCase().split('').sort().join('');
+  return arr;
 };
