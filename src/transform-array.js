c7d09a814fb07436817ec0e603df1e83f872bfc7
Vlad <vladuha011@mail.ru>
2021-03-07 18:50:54 +0300

    init: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--double-next') {
+      newArr.push(arr[i+1]);
+      continue;
+    } else if (arr[i] == '--double-prev') {
+      if (arr[i-2] == '--discard-next') {
+        continue;
+      }
+      newArr.push(arr[i-1]);
+    } else if (arr[i] == '--discard-prev') {
+      if (arr[i-2] == '--discard-next') {
+        continue;
+      }
+      newArr.pop();
+    } else if (arr[i] == '--discard-next') {
+      continue;
+    } else if (arr[i - 1] == '--discard-next') {
+      continue;
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  let newestArr = [];
+  newArr.forEach(item => {
+    if (item != undefined) {
+      newestArr.push(item);
+    }
+  });
+  return newestArr;
+}
