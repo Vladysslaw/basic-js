145ee1700fdcda9f5b99bd291f44b4f7c81006f8
Vladysslaw <vladuha011@mail.ru>
2021-03-21 13:15:04 +0300

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,83 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+  constructor(mode = true) {
+    this.mode = mode;
+  }
+  encrypt(word, keyword) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    let encryptWord = "";
+    word = word.toLowerCase();
+    for (let i = 0; i < word.length / keyword.length; i++) {
+      keyword += keyword;
+    }
+    let str = "";
+    let n = 0
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] == ' ') {
+        str += ' ';
+        n--;
+      } else {
+        str += keyword[i+n];
+      }
+    }
+    str = str.toLowerCase();
+    for (let i = 0;i < word.length;i++) {
+      if (alphabet.indexOf(word[i]) < 0) {
+        encryptWord += word[i];
+        continue;
+      }
+      if (word.charAt(i) == ' ') {
+        encryptWord += ' ';
+      } else {
+        encryptWord += alphabet.charAt((alphabet.indexOf(word.charAt(i)) + alphabet.indexOf(str.charAt(i))) % alphabet.length);
+      }
+      encryptWord = encryptWord.toUpperCase();
+    }
+  
+    if (!this.mode) {
+      encryptWord = encryptWord.split('').reverse().join('');
+      return encryptWord;
+    }
+    return encryptWord;
+    
+  }       
+  decrypt(word, keyword) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    let encryptWord = "";
+    word = word.toUpperCase();
+    for (let i = 0; i < word.length / keyword.length; i++) {
+      keyword += keyword;
+    }
+    keyword += keyword;
+    let str = "";
+    let n = 0
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] == ' ') {
+        str += ' ';
+        n--;
+      } else {
+        str += keyword[i+n];
+      }
+    }
+    str = str.toLowerCase();
+    for (let i = 0;i < word.length;i++) {
+      if (alphabet.indexOf(word[i]) < 0) {
+        encryptWord += word[i];
+        continue;
+      }
+      if (word.charAt(i) == ' ') {
+        encryptWord += ' ';
+      } else {
+        encryptWord += alphabet.charAt(((alphabet.indexOf(word.charAt(i)) + alphabet.length) - alphabet.indexOf(str.charAt(i))) % alphabet.length);
+      }
+      encryptWord = encryptWord.toUpperCase();
+    }
+    if (!this.mode) {
+      encryptWord = encryptWord.split('').reverse().join('');
+      return encryptWord;
+    }
+    return encryptWord;
     // remove line with error and write your code here
   }
 }
